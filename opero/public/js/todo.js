c19c153ba836dbeedf581a78bcf8f678a268bf4c
Patrick Nyika <patrick@reyal.biz>
2026-04-16 18:50:20 +0100

    feat: add Flow Hub button to ToDo form toolbar

--- opero/public/js/todo.js
+++ opero/public/js/todo.js
@@ -87,6 +87,7 @@ frappe.ui.form.on("ToDo", {
 			},
 		}))
 		setDueDateDescription(frm)
+		frm.add_custom_button(__("Flow Hub"), () => frappe.set_route("page", "flow-hub"))
 	},
 
 	custom_title(frm) {
